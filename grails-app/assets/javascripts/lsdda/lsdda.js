4f84193cfec6a1a95c6937357e03773393fcda7a
KulsoomPatel <kulsoom-patel@hotmail.co.uk>
2017-04-26 11:01:46 +0100

    initial Grails and Angular setup for LSDDA

---
+++ grails-app/assets/javascripts/lsdda/lsdda.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /lsdda/core/lsdda.core
+//= require /lsdda/index/lsdda.index
+
+angular.module("lsdda", [
+    "lsdda.core",
+    "lsdda.index"
+]);
