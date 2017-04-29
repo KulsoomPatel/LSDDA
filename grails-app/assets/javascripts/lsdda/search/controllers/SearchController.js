f20ed6777839466d8cf13d36242461e735bc4b89
KulsoomPatel <kulsoom-patel@hotmail.co.uk>
2017-04-29 17:06:25 +0100

    pagination woohoo

--- grails-app/assets/javascripts/lsdda/search/controllers/SearchController.js
+++ grails-app/assets/javascripts/lsdda/search/controllers/SearchController.js
@@ -2,7 +2,15 @@
  * Created by Kulsoom on 27/04/2017.
  */
 angular.module("lsdda.search")
-    .controller("SearchController", SearchController);
+    .controller("SearchController", SearchController)
+    .filter("offset", offset);
+
+
+function offset() {
+    return function (input, start) {
+        return input.slice(start);
+    };
+}
 
 
 function SearchController(SearchDataFactory) {
@@ -10,13 +18,15 @@ function SearchController(SearchDataFactory) {
     var vm = this;
     vm.searchTerms = undefined;
     vm.showTable = false;
-
-
+    vm.currentPage = 1;
+    vm.itemsPerPage = 8;
+    
     vm.searchData = function () {
 
         SearchDataFactory.list({action: 'searchData', value: vm.searchTerms}, function (response) {
             vm.results = response;
             vm.showTable = true;
+            vm.totalItems = vm.results.length;
         })
     }
 }
